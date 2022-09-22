f53b13a12211c1051ac9d0e377756aeacaab9f5b
Vattipally-Vijay-Yadav <vijayyv@azuga.com>
2022-09-22 15:31:04 +0530

    log file added

--- week3/CsvToPdf.java
+++ week3/CsvToPdf.java
@@ -15,41 +15,58 @@ import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * This class converts the csv file to pdf file
  */
 public class CsvToPdf {
-    public static void main(String[] args) throws IOException, DocumentException {
-        CSVReader reader = new CSVReader(new FileReader("/Users/azuga/Desktop/museum1.csv"));//reading csv file from given path
+
+    private static final Logger logger = LogManager.getLogger(CsvToPdf.class.getName());
+    public static void main(String[] args) throws IOException {
+        CSVReader reader;
+        try {
+            reader = new CSVReader(new FileReader("/Users/azuga/Desktop/museum1.csv"));//reading csv file from given path
+        } catch (FileNotFoundException e) {
+            logger.error("Exception "+e.getMessage());
+            throw new RuntimeException(e);
+        }
         String[] nextLine;
         Document my_pdf_data = new Document();
 
         Rectangle rc = new Rectangle(8300f,8000f);
         my_pdf_data.setPageSize(rc);
 
-
-        PdfWriter.getInstance(my_pdf_data, new FileOutputStream("/Users/azuga/Desktop/museum2.pdf"));//writing pdf file to given path
+        try {
+            PdfWriter.getInstance(my_pdf_data, new FileOutputStream("/Users/azuga/Desktop/museum2.pdf"));//writing pdf file to given path
+        } catch (DocumentException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         my_pdf_data.open();
         PdfPTable my_first_table = new PdfPTable(78);
-//        float[] columnWidths = new float[77];
-//        for (int j = 1; j <= 77; j++) {
-//            columnWidths[j] = (float) j * 2;
-//        }
-//            my_first_table.setWidths(columnWidths);
-            PdfPCell table_cell;
+        PdfPCell table_cell;
             while ((nextLine = reader.readNext()) != null) {
                 int i = 0;
+                logger.debug("Reading from csv file "+ Arrays.toString(nextLine));
                 while (i <= 77) {
                     table_cell = new PdfPCell(new Phrase(nextLine[i]));
                     my_first_table.addCell(table_cell);
                     i++;
                 }
             }
+        try {
             my_pdf_data.add(my_first_table);
-            my_pdf_data.close();
+        } catch (DocumentException e) {
+            logger.error("Exception "+e.getMessage());
+            throw new RuntimeException(e);
+        }
+        my_pdf_data.close();
         }
     }
