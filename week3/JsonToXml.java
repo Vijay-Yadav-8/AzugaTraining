b0788c547fd9d78e33b123841768aa9885d31635
Vattipally-Vijay-Yadav <vijayyv@azuga.com>
2022-09-22 15:31:39 +0530

    log file added

--- week3/JsonToXml.java
+++ week3/JsonToXml.java
@@ -10,6 +10,8 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.*;
 import org.w3c.dom.Node;
 import org.w3c.dom.bootstrap.DOMImplementationRegistry;
@@ -24,13 +26,16 @@ import javax.xml.parsers.DocumentBuilderFactory;
  */
 public class JsonToXml {
 
-        public static void main(String[] args) throws JSONException {
+    private static final Logger logger = LogManager.getLogger(JsonToXml.class.getName());
+
+        public static void main(String[] args){
         StringBuilder sb = new StringBuilder();
             sb.append("<?xml version=\"1.0\" encoding=\"ISO-8859-15\"?>\n");
             sb.append("<roots>");
             String result;
             try {
                 result = new String(Files.readAllBytes(Paths.get("/Users/azuga/Desktop/museum.json")));
+                logger.debug("Data from json file is "+result);
                 String result1 = result.replace("[{\"objectID\"","{\"objectID\"");
                 String[] arr = result1.split("},\\{\"o");
 
@@ -38,19 +43,27 @@ public class JsonToXml {
 //                file.write("");
                 for(int i=0;i< arr.length;i++) {
                     if(i==0) {
+                        logger.debug("Json Data "+arr[i]);
                         sb.append(convertToXML(arr[i] + "}", "root","root"));// This method converts json object to xml string
                         System.out.println("hi");
+
+                        logger.debug("Xml Data is "+sb);
                     }
                         else if (i==arr.length-1) {
+
+                        logger.debug("Json Data "+arr[i]);
                         System.out.println(arr[i].charAt(arr[i].length()-1));
                         result="{\"o"+arr[i].replace("\"GalleryNumber\":\"\"}]","\"GalleryNumber\":\"\"}");
                         sb.append(convertToXML(result, "root","root"));//file.append(convertToXML(arr[i], "root"));
                         System.out.println("hello");
+                        logger.debug("Xml Data is "+sb);
                     }
                     else{
-                        System.out.println(i);
+
+                        logger.debug("Json Data "+arr[i]);
                         result="{\"o"+arr[i]+"}";
                         sb.append(convertToXML(result, "root","root"));
+                        logger.debug("Xml Data is "+sb);
 
 
                     }
@@ -63,11 +76,12 @@ public class JsonToXml {
 
 
             } catch (IOException e1) {
-
+                logger.error("Exception "+e1.getMessage());
                 e1.printStackTrace();
             }
         }
     public static String format(String xml) {
+            logger.info("format() is invoked");
 
         try {
             final InputSource src = new InputSource(new StringReader(xml));
@@ -83,9 +97,10 @@ public class JsonToXml {
 
             writer.getDomConfig().setParameter("format-pretty-print", Boolean.TRUE); // Set this to true if the output needs to be beautified.
             writer.getDomConfig().setParameter("xml-declaration", keepDeclaration); // Set this to true if the declaration is needed to be outputted.
-
+            logger.debug("formatted data is"+writer.writeToString(document));
             return writer.writeToString(document);
         } catch (Exception e) {
+            logger.error("Exception "+e.getMessage());
             throw new RuntimeException(e);
         }
     }
@@ -98,6 +113,7 @@ public class JsonToXml {
      * @throws JSONException - to handle syntax error of json string
      */
         public static String convertToXML(String jsonString, String root,String root1) throws JSONException {    // handle JSONException
+            logger.info("convertToXml() is invoked");
             JSONObject jsonObject =new JSONObject(jsonString);
             String unformattedXml =  "<"+root+">" + XML.toString(jsonObject) + "</"+root1+">";
             return format(unformattedXml);
