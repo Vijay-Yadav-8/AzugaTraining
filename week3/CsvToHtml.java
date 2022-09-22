892cf166808457baf55b895a22871f784835e7d4
Vattipally-Vijay-Yadav <vijayyv@azuga.com>
2022-09-22 15:30:51 +0530

    log file added

--- week3/CsvToHtml.java
+++ week3/CsvToHtml.java
@@ -7,6 +7,9 @@
 
 package com.azuga.training.week3;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -18,20 +21,24 @@ import java.util.List;
  * This class converts csv file to html file
  */
 public class CsvToHtml {
+
+    private static final Logger logger = LogManager.getLogger(CsvToHtml.class.getName());
     public static void main(String[] args) {
         List<String> lines = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("/Users/azuga/Desktop/museum1.csv"))) {//reading the file here
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
 
+                logger.debug("Data before manipulation"+currentLine);
                 String s2 = currentLine.replaceAll("\"\"","null");
                 String s3 = s2.replaceAll("\"","");
                 String s=s3.replaceAll("https\\://images.metmuseum.org/CRDImages","<img src=https\\://images.metmuseum.org/CRDImages");
                 String s1=s.replaceAll("jpg","jpg style=\"width:100px;height:100px;\" >");
-
+                logger.debug("Data after manipulation"+s1);
                 lines.add(s1);
             }
         } catch (IOException e) {
+            logger.error("Exception "+e.getMessage());
             e.printStackTrace();
         }
         for (int i = 0; i < lines.size(); i++) {
@@ -43,9 +50,11 @@ public class CsvToHtml {
 
         try (FileWriter writer = new FileWriter("/Users/azuga/Desktop/museum3.html")) {//writing String to html file
             for (String line : lines) {
+                logger.debug("data which is converted to html form "+line);
                 writer.write(line + "\n");
             }
         } catch (IOException e) {
+            logger.error("Exception "+e.getMessage());
             e.printStackTrace();
         }
     }
