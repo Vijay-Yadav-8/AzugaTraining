1924ebabc3e66a783a497c494207292defd22cf2
Vattipally-Vijay-Yadav <vijayyv@azuga.com>
2022-09-27 20:39:05 +0530

    Interface for ChartsMaker class

---
+++ ChartMake.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2022.  - All Rights Reserved
+ *  * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ *  * is strictly prohibited-
+ *  * @Author -vijayyv.
+ */
+
+package com.azuga.training;
+
+/**
+ * This interface is used to prepare charts
+ */
+public interface ChartMake {
+
+    void pieChartMaker();
+    void lineGraphMaker(String srcPath);
+    void barGraphMaker();
+}
