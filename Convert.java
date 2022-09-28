ed37d3a36dbecc96650bd5e78121189d391514cb
Vattipally-Vijay-Yadav <vijayyv@azuga.com>
2022-09-28 18:22:30 +0530

    Interface for converter.class

--- Convert.java
+++ Convert.java
@@ -8,12 +8,28 @@
 package com.azuga.training;
 
 import java.io.IOException;
+
 /**
  * This method is used to convert one file to other file
  */
-public interface Convert{
-    void csvToHtml(String src,String dest);
-    void csvToPdf(String src,String des) throws IOException;
-    void jsonToXml(String src,String desh);
-    void csvToExcel(String src,String des);
+public interface Convert {
+    /**
+     * This method converts the csv file to html format
+     */
+    void csvToHtml(String src, String dest);
+
+    /**
+     * This method converts the csv file to pdf format
+     */
+    void csvToPdf(String src, String des) throws IOException;
+
+    /**
+     * This method converts the json file to xml format
+     */
+    void jsonToXml(String src, String des);
+
+    /**
+     * This method converts the csv file to xlsx format
+     */
+    void csvToExcel(String src, String des);
 }
