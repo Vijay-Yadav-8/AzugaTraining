c97a4b6c348b7799c96d03ac05918b4a0599bd06
Vattipally-Vijay-Yadav <vijayyv@azuga.com>
2022-09-28 18:23:20 +0530

    Used to fetch museum api data and save it to csv file

--- MuseumApi.java
+++ MuseumApi.java
@@ -11,6 +11,7 @@ package com.azuga.training;
 import com.github.opendevl.JFlat;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -22,86 +23,93 @@ import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+
 /**
  * This class is used to fetch the data from museum api as json string and appends to csv file
  */
 public class MuseumApi {
     private static final Logger logger = LogManager.getLogger(MuseumApi.class.getName());
-public void jsonWriter(String srcPath,String destPath){
 
-    PrintWriter pw;
-    try {
-        pw = new PrintWriter(srcPath);
-    } catch (FileNotFoundException e) {
-        logger.error("Exception "+e.getMessage());
-        throw new RuntimeException(e);
-    }
-    for(int i=1;i<=10;i++) {
-        String url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + i*10;
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
-        HttpClient client = HttpClient.newBuilder().build();
-        HttpResponse<String> response;
+    public void jsonWriter(String srcPath, String destPath) {
+        long start = System.currentTimeMillis();
+        PrintWriter pw = null;
         try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            logger.error("Exception "+e.getMessage());
-            throw new RuntimeException(e);
+            pw = new PrintWriter(srcPath);
+        } catch (FileNotFoundException e) {
+            logger.error("{} occurred while accessing file from {}", e, srcPath);
         }
-        logger.info("making a call on url");
-        if (response.statusCode() == 200) {
-            logger.info("connected to server..✦..Fetching the data.✦..✦..");
-            pw.write(response.body().replaceAll("\\[]", "null"));
-        }else
-            logger.info("connection lost 😔");
-    }
-    pw.close();
-    String json;
-    try {
-        json = new String(Files.readAllBytes(Paths.get(srcPath)));
-    } catch (IOException e) {
-        logger.error("Exception "+e.getMessage());
-        throw new RuntimeException(e);
-    }
-    String jsonn = json.replaceAll("}\\{","},{");
-    String s = "[" +
-            jsonn +
-            "]";
-    JFlat flatMe = new JFlat(s);
-    try {
-        flatMe.json2Sheet().headerSeparator("_").write2csv(destPath);
-    } catch (Exception e) {
-        logger.error("Exception "+e.getMessage());
-        throw new RuntimeException(e);
-    }
-    logger.info("\nFile created");
-    try {
-        CSVReader reader = new CSVReader(new FileReader(destPath));
-        List<String[]> csvBody  =reader.readAll();
+        Random rm = new Random();
+        if (pw != null) {
+            for (int i = 1; i <= 10; i++) {
+                String url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + rm.nextInt(481501);
+                logger.info("Making the call on url: {}", url);
+                HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+                HttpClient client = HttpClient.newBuilder().build();
+                HttpResponse<String> response = null;
+                try {
+                    response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                } catch (IOException | InterruptedException e) {
+                    logger.error("{} occurred while getting a response museum api url {}", e, url);
+                }
+                if (response != null) {
+                    if (response.statusCode() == 200) {
+                        logger.debug("Response from server is :{}", response.body());
+                        pw.write(response.body().replaceAll("\\[]", "null"));
+                    } else
+                        logger.info("Museum api responded with ErrorCode :{}", response.statusCode());
+                } else
+                    logger.info("Response from Museum api is null");
+            }
+            pw.close();
+        }
+        String json = null;
+        try {
+            json = new String(Files.readAllBytes(Paths.get(srcPath)));
+        } catch (IOException e) {
+            logger.error("{} occurred while reading file from {}", e, srcPath);
+        }
+        if (json != null) {
+            String jsonn = json.replaceAll("}\\{", "},{");
+            String s = "[" +
+                    jsonn +
+                    "]";
+            JFlat flatMe = new JFlat(s);
+            try {
+                flatMe.json2Sheet().headerSeparator("_").write2csv(destPath);
+            } catch (Exception e) {
+                logger.error("{} occurred while writing file to {}", e, destPath);
+            }
+            try {
+                CSVReader reader = new CSVReader(new FileReader(destPath));
+                logger.info("Reading file from Specified path i.e, {}", destPath);
+                List<String[]> csvBody = reader.readAll();
 
-        for(int m=1 ; m<csvBody.size();m++) {
-            String i = csvBody.get(m)[0].replaceAll(".0+$", "");
-            String j = csvBody.get(m)[29].replaceAll(".0+$", "");
-            String k = csvBody.get(m)[30].replaceAll(".0+$", "");
-            csvBody.get(m)[0] = i;
-            csvBody.get(m)[29] = j;
-            csvBody.get(m)[30] = k;
-        }
-        for(int m=0 ; m<csvBody.size();m++) {
-            int n=m+1;
-            while (n< csvBody.size()&&(csvBody.get(m)[0]).equals(csvBody.get(n)[0])) {
-                String[] i=csvBody.get(m);
-                csvBody.remove(i);
+                for (int m = 1; m < csvBody.size(); m++) {
+                    String i = csvBody.get(m)[0].replaceAll(".0+$", "");
+                    String j = csvBody.get(m)[29].replaceAll(".0+$", "");
+                    String k = csvBody.get(m)[30].replaceAll(".0+$", "");
+                    csvBody.get(m)[0] = i;
+                    csvBody.get(m)[29] = j;
+                    csvBody.get(m)[30] = k;
+                }
+                for (int m = 0; m < csvBody.size(); m++) {
+                    int n = m + 1;
+                    while (n < csvBody.size() && (csvBody.get(m)[0]).equals(csvBody.get(n)[0])) {
+                        String[] i = csvBody.get(m);
+                        csvBody.remove(i);
+                    }
+                }
+                reader.close();
+                logger.trace("opening file on {}", destPath);
+                CSVWriter writer = new CSVWriter(new FileWriter(destPath));
+                writer.writeAll(csvBody);
+                writer.close();
+                logger.trace("closing file on {}", destPath);
+            } catch (CsvException | IOException e) {
+                logger.error("{} occurred while writing file to {}", e, destPath);
             }
         }
-        reader.close();
-        CSVWriter writer = new CSVWriter(new FileWriter(destPath));
-        writer.writeAll(csvBody);
-        writer.close();
-        logger.info("Data got modified");
+        long end = System.currentTimeMillis();
+        logger.info("jsonWriter() is executed in {} ms", (end - start));
     }
-    catch (Exception e){
-        logger.warn("Exception "+e.getMessage());
-        e.printStackTrace();
-    }
-}
 }
