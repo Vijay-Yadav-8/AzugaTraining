4f8a8bae0ccc821402ed6844a8507bd5cd3d5274
Vattipally-Vijay-Yadav <vijayyv@azuga.com>
2022-11-17 16:09:52 +0530

    Example class for jenkins testing

---
+++ controller/JenkinsExample.java
@@ -0,0 +1,12 @@
+package com.azuga.museum.controller;
+
+/**
+ * This class is used as example for jenkins
+ */
+public class JenkinsExample {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello-World");
+    }
+}
